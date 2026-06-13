fcf3e93213481d0811803ed24cfd222c6636fe84
Richard-H-Y <vxrqlo42@gmail.com>
2026-06-13 13:17:01 +0700

    models user

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,37 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    protected $fillable = ['name', 'email', 'password', 'role'];
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
 
-    protected $hidden = ['password'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
 
-    public function orders()
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return $this->hasMany(Order::class);
-    }
-    public function carts()
-    {
-        return $this->hasMany(Cart::class);
-    }
-    public function wishlists()
-    {
-        return $this->hasMany(Wishlist::class);
-    }
-    public function reviews()
-    {
-        return $this->hasMany(Review::class);
-    }
-    public function addresses()
-    {
-        return $this->hasMany(Address::class);
-    }
-    public function isAdmin()
-    {
-        return $this->role === 'admin';
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
